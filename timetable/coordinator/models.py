b9caad443ae5813a365327878ab699285cbc2f1d
AmitThakkar05 <athakkar429@gmail.com>
2022-06-30 13:35:05 +0530

    timetable generator

--- timetable/coordinator/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class courseTable(models.Model):
-    courseID = models.CharField(max_length=5)
-    courseName = models.CharField(max_length=255)
-    credit = models.CharField(max_length=5)
-    batch = models.CharField(max_length=10)
